1ec6d36634a038d0e77e91edf54411badb6e386a
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-03-04 23:19:45 -0600

    basic forgot password component complete

---
+++ src/components/ForgotPassword.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router";
+
+const ForgotPassword = () => {
+  return (
+    <>
+      <div className="grid h-screen text-center">
+        <form onSubmit="" className="grid place-content-center gap-5">
+          <h1>Forgot Password</h1>
+          <div className="flex flex-col">
+            <label className="text-left" htmlFor="username">
+              Email
+            </label>
+            <input type="email" id="username" />
+          </div>
+
+          <div className="flex flex-col gap-3 items-center">
+            <input className="button" type="submit" value="Send Verification" />
+            <Link className="button" to="/">
+              Back
+            </Link>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ForgotPassword;
