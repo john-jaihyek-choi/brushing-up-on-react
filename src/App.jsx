c09cccddded3f3c4b8718811ac633789c6b67d80
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-03-04 23:11:58 -0600

    basic css update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router";
 import Login from "./components/Login";
 
-import "./App.css";
-
 const App = () => {
   return (
     <div className="flex flex-col gap-2 justify-center items-center h-screen">
