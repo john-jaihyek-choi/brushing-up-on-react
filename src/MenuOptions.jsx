9322d6bc14925b56d4ebce0c63dfb3e04fdd8172
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-03-06 02:30:22 -0600

    map id bug fix

--- src/MenuOptions.jsx
+++ src/MenuOptions.jsx
@@ -19,7 +19,7 @@ const MenuOptions = () => {
   return (
     <div className="flex flex-col gap-2 justify-center items-center h-screen">
       {components.map(({ path, name }) => (
-        <Link id={name} to={path} className="button max-w-100">
+        <Link key={name} id={name} to={path} className="button max-w-100">
           {name}
         </Link>
       ))}
