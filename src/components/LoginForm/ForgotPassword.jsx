ae69b97fb1ce97ef2da3856304b9c7eb2d75f4fb
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-03-06 02:30:08 -0600

    home button import removed

--- src/components/LoginForm/ForgotPassword.jsx
+++ src/components/LoginForm/ForgotPassword.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router";
-import HomeButton from "../HomeButton";
 
 const ForgotPassword = () => {
   return (
--- src/components/LoginForm/Login.jsx
+++ src/components/LoginForm/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
-import HomeButton from "../HomeButton";
+
 import "./loginForm.css";
 
 const Login = () => {
