232f4094c8c9b2bef41bbadda500f2d508c62807
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-03-05 23:00:57 -0600

    menuoptions component separated to own module

---
+++ src/MenuOptions.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+const components = [
+  {
+    path: "/login",
+    name: "Login Form",
+  },
+  {
+    path: "/api-fetch-methods",
+    name: "API Fetch Methods",
+  },
+  {
+    path: "/flexbox",
+    name: "Flexbox",
+  },
+];
+
+const MenuOptions = () => {
+  return (
+    <div className="flex flex-col gap-2 justify-center items-center h-screen">
+      {components.map(({ path, name }) => (
+        <Link id={name} to={path} className="button max-w-100">
+          {name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+export default MenuOptions;
