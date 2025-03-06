caf95e7484f3f71658ba708a138e47933a2493b8
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-03-05 23:04:16 -0600

    homebutton module removed and directly implemented in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router";
-import HomeButton from "./components/HomeButton";
+import { Link, Outlet } from "react-router";
+import { Home } from "lucide-react";
 import "./app.css";
 
 const App = () => {
   return (
     <>
       <div className="flex flex-col h-screen">
-        <HomeButton />
+        <div className="sticky m-2">
+          <Link to="/">
+            <Home size={24} />
+          </Link>
+        </div>
+
         <Outlet />
       </div>
     </>
