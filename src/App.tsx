bc32bec393ebb8fccef1d485835f2d4f73a16a90
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-03-12 19:49:43 -0500

    home button combined to navigator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router";
-import HomeButton from "./components/HomeButton/HomeButton";
+import Navigators from "./components/Navigators/Navigators";
 import "./app.css";
 
 const App = () => {
   return (
     <>
       <div className="flex flex-col h-screen text-center gap-5">
-        <HomeButton />
+        <Navigators />
         <Outlet />
       </div>
     </>
