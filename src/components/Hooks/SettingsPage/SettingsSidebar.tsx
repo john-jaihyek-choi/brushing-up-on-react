a2d6adf1eb47e45a6b5d186c9796df7bb70485fe
john-jaihyek-choi <jaihyek.choi@gmail.com>
2025-04-16 23:58:34 -0500

    setting side bar and theme toggle starter

---
+++ src/components/Hooks/SettingsPage/SettingsSidebar.tsx
@@ -0,0 +1,20 @@
+const SettingsSidebar = () => {
+  /* 
+Info needed:
+    - Username
+        - read/update
+
+Note:
+    - will make username editable
+
+Design:
+    - close button
+        - visibility to hide then main margin back to 0
+    - Username with an icon
+        - editable name next to it
+    - blank options
+    - theme toggler at the very bottom
+*/
+  return <div>SettingsSidebar</div>;
+};
+export default SettingsSidebar;
---
+++ src/components/Hooks/SettingsPage/ThemeToggle.tsx
@@ -0,0 +1,16 @@
+const ThemeToggle = () => {
+  /* 
+Info needed:
+    - theme
+        - actions
+            - read/update
+
+Components:
+    - None
+
+Design:
+    - theme toggler
+*/
+  return <div>ThemeToggle</div>;
+};
+export default ThemeToggle;
